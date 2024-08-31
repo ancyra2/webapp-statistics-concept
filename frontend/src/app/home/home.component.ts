9fd7b56223c01536a739c0ee767cf337b730209c
Talih Duran <duran.talih@gmail.com>
2024-08-31 14:30:28 +0300

    feat: add hera to home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../shared/search-bar/search-bar.component';
 import { SearchLinksComponent } from '../shared/search-links/search-links.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchBarComponent, SearchLinksComponent],
+  imports: [SearchBarComponent, SearchLinksComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
