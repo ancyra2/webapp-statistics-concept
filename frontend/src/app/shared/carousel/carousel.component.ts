d8a2d20434e8448db0a924ef0fbcbe69fcfe7bce
ancyra2 <duran.talih@gmail.com>
2024-09-01 15:43:30 +0300

    feat: add carousel to home component

--- frontend/src/app/shared/carousel/carousel.component.ts
+++ frontend/src/app/shared/carousel/carousel.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { LastStudiesComponent } from '../last-studies/last-studies.component';
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, LastStudiesComponent],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
 export class CarouselComponent {
-
+ @Input() width: string = '100%'; 
+ @Input() height: string = '300px'; 
 }
