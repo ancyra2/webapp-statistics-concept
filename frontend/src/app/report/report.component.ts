86a0b95dfb212a926e7305aa48642a27df74a742
ancyra2 <duran.talih@gmail.com>
2024-09-06 11:44:37 +0300

    feat: add Google Charts to project with no packages, only with api

--- frontend/src/app/report/report.component.ts
+++ frontend/src/app/report/report.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DescriptiveTableComponent } from '../shared/descriptive-table/descriptive-table.component';
+
+declare var google: any;  // Google Charts'ın global API'sine erişmek için
+
 @Component({
   selector: 'app-report',
   standalone: true,
@@ -7,6 +10,32 @@ import { DescriptiveTableComponent } from '../shared/descriptive-table/descripti
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
-export class ReportComponent {
+export class ReportComponent implements OnInit{
+  ngOnInit(): void {
+    this.loadGoogleCharts();
+  }
 
+  loadGoogleCharts() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(this.drawChart);
+  }
+
+  drawChart = () => {
+    const data = google.visualization.arrayToDataTable([
+      ['Browser', 'Percentage'],
+      ['Chrome', 62.74],
+      ['Safari', 13.39],
+      ['Firefox', 10.85],
+      ['Edge', 4.67],
+      ['Others', 8.35]
+    ]);
+
+    const options = {
+      title: 'Browser Market Share',
+      pieHole: 0.4,
+    };
+
+    const chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
+  }
 }
